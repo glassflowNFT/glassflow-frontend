94a68a461b42d3132214d06b2a7f717191d6755d
MickNorris <lorem.ipsum.crypto@gmail.com>
2022-01-19 14:05:40 -0500

    added logic for navigating to age verification page and saving session cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home/Home';
 import Verification from './pages/Verification/Verification';
+import { useNavigate } from 'react-router';
+import cookie from 'react-cookies';
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+
+    // load age verification cookie
+    const verificationCookie = cookie.load('age-verified');
+
+    // check if cookie exists
+    if (!verificationCookie) {
+      // setup cookie expiration
+      const expires = new Date();
+      expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14);
+      // save cookie
+      cookie.save(
+        'age-verified',
+        'true',
+        {
+          expires
+        }
+      );
+      // navigate to the verify page on initial visit
+      navigate("/verify");
+    }
+
+  }
+
+  useEffect(() => {
+    handleNavigation();
+  }, []);
+
   return (
-    <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/verify" element={<Verification/>} />
         </Routes>
       </div>
-    </BrowserRouter>
   );
 }
 
