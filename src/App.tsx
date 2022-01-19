9dfca11a9972ed1adfa4c1a419d5ebd07f205c8a
MickNorris <lorem.ipsum.crypto@gmail.com>
2022-01-19 14:45:48 -0500

    added navbar and some basic routing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import Home from './pages/Home/Home';
 import Verification from './pages/Verification/Verification';
 import { useNavigate } from 'react-router';
 import cookie from 'react-cookies';
+import Nav from './components/Nav/Nav';
+import Mint from './pages/Mint/Mint';
+import User from './pages/User/User';
+import Support from './pages/Support/Support';
+import Search from './pages/Search/Search';
 
 function App() {
   const navigate = useNavigate();
 
+  // TODO: only save cookie after user validates age
   const handleNavigation = () => {
 
     // load age verification cookie
@@ -35,12 +41,18 @@ function App() {
 
   useEffect(() => {
     handleNavigation();
+    // eslint-disable-next-line
   }, []);
 
   return (
       <div className="App">
+        <Nav/>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/mint" element={<Mint/>} />
+          <Route path="/user" element={<User/>} />
+          <Route path="/support" element={<Support/>} />
+          <Route path="/search" element={<Search/>} />
           <Route path="/verify" element={<Verification/>} />
         </Routes>
       </div>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,18 @@
+import { Home, PlusCircle, User, HelpCircle, Search } from 'react-feather';
+import { Link } from "react-router-dom";
+import "./nav.css"
+
 export default function Nav() {
   return(
-    <nav></nav>
+    <nav className="nav-wrapper">
+      <span>GlassFlow</span>
+      <ul>
+        <li><Link to="/"><Home/> Home</Link></li> 
+        <li><Link to="/mint"><PlusCircle/> Mint</Link></li> 
+        <li><Link to="/user"><User/> Wallet</Link></li> 
+        <li><Link to="/support"><HelpCircle/> Support</Link></li> 
+        <li><Link to="/search"><Search/> Search</Link></li> 
+      </ul>
+    </nav>
   )
 }
