72be961e14e6a635a133cff9713e2132bc708108
MickNorris <lorem.ipsum.crypto@gmail.com>
2022-01-24 12:43:22 -0500

    added auth toggle text

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,22 +2,36 @@ import { Fragment } from "react";
 import "./login.css";
 
 export default function Login() {
+
+  const toggleAuthSection = (e: any) => {
+    e.preventDefault();
+  }
+
   return (
     <Fragment>
       <section className="auth-left">
-        <div className="auth-header"> 
+        <div className="auth-header">
           <h1>Login</h1>
-          <span><b>*</b> Required Fields</span>
+          <span>
+            <b>*</b> Required Fields
+          </span>
         </div>
         <section className="auth-field-section">
-          <span>Email <b>*</b></span>
+          <span>
+            Email <b>*</b>
+          </span>
           <input placeholder="your_email@email.com"></input>
         </section>
         <section className="auth-field-section">
-          <span>Password <b>*</b></span>
+          <span>
+            Password <b>*</b>
+          </span>
           <input placeholder="********"></input>
         </section>
         <button className="primary-button">Login</button>
+        <a href="/" onClick={toggleAuthSection} className="toggle-auth">
+          <span className="secondary">No account yet? Signup here</span>
+        </a>
       </section>
     </Fragment>
   );
