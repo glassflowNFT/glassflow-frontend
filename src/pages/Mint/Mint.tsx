461815a5cf1a2a46984571023e20d705baa2567c
MickNorris <lorem.ipsum.crypto@gmail.com>
2022-01-26 13:00:33 -0500

    fixed some sizing issues on smaller screens, fixed outline rules, added user page

--- src/pages/Mint/Mint.tsx
+++ src/pages/Mint/Mint.tsx
@@ -1,8 +1,35 @@
+import React, { useState } from "react";
 import { ChevronDown, Image, PlusCircle } from "react-feather";
 import "./mint.css";
 
 export default function Mint() {
-  return(
+
+  const [creators, setCreators] = useState<[string, number][]>([["", 100]]);
+
+  // update any changes
+  const updateCreators = (e: any, index: number, id: number) => {
+    // make copy of creators state var
+    let newCreators = [...creators];
+    // update creator wallet address or revenue
+    if (id === 0) {
+      const oldData = creators[index];
+      newCreators[index] = [e.target.value, oldData[1]];
+    } else {
+      const oldData = creators[index];
+      newCreators[index] = [oldData[0], e.target.value];
+    }
+    // update state
+    setCreators(newCreators);
+  }
+
+  // add a new creator + revenue share to the list
+  const addNewCreator = () => {
+    let newCreators = [...creators];
+    newCreators.push(["", 0]);
+    setCreators(newCreators);
+  }
+
+  return (
     <div className="mint-wrapper page-wrapper">
       <div className="mint-header">
         <h1>Mint New Item</h1>
@@ -11,60 +38,103 @@ export default function Mint() {
         </span>
       </div>
       <section className="auth-field-section">
-        <span>
-          Image or Video
-        </span>
+        <span>Image or Video</span>
         <div className="file-selector-wrapper">
-          <input className="file-selector" type="file" accept=".jpg, .jpeg, .png"></input>
-          <Image/>
+          <input
+            className="file-selector"
+            type="file"
+            accept=".jpg, .jpeg, .png"
+          ></input>
+          <Image />
         </div>
-        <span className="secondary hint">File types supported: JPG, JPEG, PNG</span>
+        <span className="secondary hint">
+          File types supported: JPG, JPEG, PNG
+        </span>
       </section>
       <section className="mint-input-section">
-        <span>Name <b>*</b></span>
+        <span>
+          Name <b>*</b>
+        </span>
         <input placeholder="Item name"></input>
       </section>
       <section className="mint-input-section">
         <span>External Link</span>
-        <span className="hint secondary">This link will be displayed on the item’s detail page so that users can learn more about your item. You are free to put any link to any page or content that you see fit.</span>
+        <span className="hint secondary">
+          This link will be displayed on the item’s detail page so that users
+          can learn more about your item. You are free to put any link to any
+          page or content that you see fit.
+        </span>
         <input placeholder="https://link.com/path/to/data" type="url"></input>
       </section>
       <section className="mint-input-section">
         <span>Description</span>
-        <span className="hint secondary">The description will be included on the item's detail page underneath its image</span>
+        <span className="hint secondary">
+          The description will be included on the item's detail page underneath
+          its image
+        </span>
         <textarea placeholder="Detailed description of your item"></textarea>
       </section>
       <section className="mint-input-section">
         <span>Collection</span>
-        <span className="hint secondary">This is the collection your item will be grouped with</span>
+        <span className="hint secondary">
+          This is the collection your item will be grouped with
+        </span>
         <div className="collection-dropdown">
           <span>Select Collection</span>
-          <ChevronDown/>
+          <ChevronDown />
         </div>
       </section>
       <section className="mint-input-section">
-        <span>Number of real piece representations <b>*</b></span>
-        <span className="hint secondary">How many real world items are associated with this NFT</span>
+        <span>
+          Number of real piece representations <b>*</b>
+        </span>
+        <span className="hint secondary">
+          How many real world items are associated with this NFT
+        </span>
         <input placeholder="1"></input>
       </section>
       <section className="mint-input-section">
-        <span>Number of collectable nfts (non representing real piece) <b>*</b></span>
+        <span>
+          Number of collectable nfts (non representing real piece) <b>*</b>
+        </span>
         <span className="hint secondary">How many NFTs should be created</span>
         <input placeholder="1"></input>
       </section>
       <section className="mint-input-section">
-        <span>Creators <b>*</b></span>
-        <span className="hint secondary">How many people were involved in the creation of this piece and what is their royalty percentage</span>
-        <div className="creator-wrapper">
-          <input className="creator-input" placeholder="Creator wallet address"></input> 
-          <input className="revenue-input" type="number" min="0" placeholder="% Revenue distribution"></input> 
-        </div>
-        <button className="new-creator-button"><PlusCircle/> Add new creator</button>
+        <span>
+          Creators <b>*</b>
+        </span>
+        <span className="hint secondary">
+          How many people were involved in the creation of this piece and what
+          is their revenue distribution percentage 
+        </span>
+        {creators.map((_creator, index) => (
+          <div className="creator-wrapper">
+            <input
+              className="creator-input"
+              placeholder="Creator wallet address"
+              onChange={(e: any) => updateCreators(e, index, 0)}
+            ></input>
+            <input
+              className="revenue-input"
+              type="number"
+              min="0"
+              max="100"
+              placeholder="% Revenue distribution"
+              onChange={(e: any) => updateCreators(e, index, 1)}
+            ></input>
+          </div>
+        ))}
+        <button className="new-creator-button" onClick={addNewCreator}>
+          <PlusCircle /> Add new creator
+        </button>
       </section>
       <section className="mint-input-section">
-        <span>Initial ask price <b>*</b></span>
+        <span>
+          Initial ask price <b>*</b>
+        </span>
         <span className="hint secondary">Initial price for the piece</span>
-        <input placeholder="1"></input>
+        <input placeholder="1" type="number" min="0"></input>
       </section>
 
       <button className="primary-button">Create Item</button>
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,7 +1,88 @@
+import { generateSentences, generateWords } from "../../components/LoremIpsum";
+import gradient from 'random-gradient';
+import "./user.css";
+import { useState } from "react";
+import { Search } from "react-feather";
+
+const count = Math.round(Math.random() * (500 - 0) + 0);
+const bgGradient = { background: gradient(count.toString()) };
+const words = generateWords(2);
+const sentence = generateSentences(5);
+
 export default function User() {
-  return(
-    <div>
-      user page
+
+
+  const [selectedFilter, setSelectedFilter] = useState<string>("owned");
+
+  const setFilter = (e: any) => {
+    setSelectedFilter(e.target.innerHTML.toLowerCase());
+  }
+
+  const renderTrendingArtists = () => {
+    const count = Math.round(Math.random() * (500 - 0) + 0);
+    const bgGradient = { background: gradient(count.toString()) };
+    return (
+      <div className="content" key={count}>
+        <div className="image-wrapper" style={bgGradient}>
+          {/*<img src={`https://picsum.photos/200/${count}`} alt="artist"></img>*/}
+        </div>
+        <div className="content-info">
+          <span className="content-name">{generateWords(2)}</span>
+          {/*<span className="content-bio secondary">{generateSentences(1)}</span>*/}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="user-wrapper page-wrapper">
+      <section className="user-info-wrapper">
+        <div className="profile-wrapper" style={bgGradient}></div>
+        <section className="user-info">
+          <div>
+            <span className="user-name">{words}</span>
+            <span className="user-address secondary">
+              (0xeD470d85A7FD0f2b558A7bbFADb2338A7c03F51A)
+            </span>
+          </div>
+          <p className="user-bio secondary">{sentence}</p>
+        </section>
+      </section>
+      <section className="content-filter-wrapper">
+        <button
+          className={`content-filter ${
+            selectedFilter === "owned" ? "active" : ""
+          }`}
+          onClick={setFilter}
+        >
+          Owned
+        </button>
+        <button
+          className={`content-filter ${
+            selectedFilter === "created" ? "active" : ""
+          }`}
+          onClick={setFilter}
+        >
+          Created
+        </button>
+        <button
+          className={`content-filter ${
+            selectedFilter === "listed" ? "active" : ""
+          }`}
+          onClick={setFilter}
+        >
+          Listed
+        </button>
+      </section>
+      <section className="user-content">
+        <div className="search-wrapper">
+          <Search/>
+          <input placeholder="Search for items"></input>
+        </div>
+        <section className="content-wrapper">
+        {[...Array(Math.round(Math.random() * (20 - 1) + 1))].map(renderTrendingArtists)}
+        </section>
+      </section>
     </div>
   );
 }
