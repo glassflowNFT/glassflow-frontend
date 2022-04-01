ca97d987c80791315d579d26a59efab53b705943
lorem ipsumm <lorem.ipsum.crypto@gmail.com>
2022-04-01 11:15:32 -0400

    layout for genesis page

--- src/pages/Genesis/Genesis.tsx
+++ src/pages/Genesis/Genesis.tsx
@@ -2,6 +2,7 @@ import { generateSentences, generateWords } from "../../components/LoremIpsum";
 import gradient from 'random-gradient';
 import { uid } from "uid";
 import "./genesis.css";
+import { LinearProgress } from "@material-ui/core";
 
 export default function Genesis() {
 
@@ -10,6 +11,7 @@ export default function Genesis() {
       [...new Array(5)].map((val, index: number) => {
         const id = uid();
         const bgGradient = { background: gradient(id.toString()) };
+        const percent = Math.floor(Math.random() * 100);
         return(
           <div key={index} className="launch-wrapper">
             <section className="launch-wrapper-left">
@@ -19,7 +21,26 @@ export default function Genesis() {
               <p className="launch-description secondary">
                 {generateSentences(3)}
               </p>
-              <span className="launch-creator secondary">Created By: <b>wasm14f3....wet2fg</b></span>
+              <div className="launch-stats">
+                <div className="launch-stat-wrapper">
+                  <span className="launch-stat">Created By:</span>
+                  <span className="launch-stat">wasm1baa6....cb4a</span>
+                </div>
+                <div className="launch-stat-wrapper">
+                  <span className="launch-stat">Number of NFTs:</span>
+                  <span className="launch-stat">415</span>
+                </div>
+                <div className="launch-stat-wrapper">
+                  <span className="launch-stat">Mint Price:</span>
+                  <span className="launch-stat">13.3 CHT</span>
+                </div>
+                <div className="launch-stat-wrapper no-margin">
+                  <span className="launch-stat">Percent Minted:</span>
+                  <span className="launch-stat">{percent}%</span>
+                </div>
+                <LinearProgress variant="determinate" value={percent} />
+              </div>
+              <button className="primary-button">Mint</button>
             </section>
             <section className="launch-wrapper-right">
               <div className="image-wrapper" style={bgGradient}></div>
